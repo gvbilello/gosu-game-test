8be894fd330836e8a77a6690a7611fcacdb3dc51
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-12-08 15:09:18 -0500

    Working on running and idle animations.

--- test1.rb
+++ test1.rb
@@ -1,28 +1,70 @@
 require 'gosu'
 
+require 'pry'
+require 'byebug'
+
+
+def media_path(file)
+	File.join(File.dirname(File.dirname(__FILE__)), 'media', file)
+end
+
 class Sprite
+
+	FRAME_DELAY = 10
+	SPRITE = media_path('characters.png')
+	TILE_WIDTH = 32
+	# def self.load_animation(window)
+  #   Gosu::Image.load_tiles(window, SPRITE, 32, 32, false)
+  # end
 
 	def initialize(window)
 		@window = window
-		@sprite = Gosu::Image.new(@window, "characters.png")
-		@x = @window.width / 2
-		@y = @window.height - 66
+		@animation = Gosu::Image.load_tiles(@window, SPRITE, 32, 32, false)
+		@x = @window.height / 2
+		@y = @window.height - (TILE_WIDTH * 2)
 		@direction = :right
+		@current_frame = 23
 	end
 
 	def update
-		if @window.button_down?(Gosu::KbLeft)
-			@direction = :left
-			@x -= 5
-		elsif @window.button_down?(Gosu::KbRight)
-			@direction = :right
-			@x += 5
+		# idle jump animation
+		if @window.button_down?(Gosu::KbUp)
+			# jump
+		end
+
+		# left run animation & change direction from right to left
+		if @window.button_down?(Gosu::KbLeft) && @x > (TILE_WIDTH * 2)
+			if @direction == :right
+				@direction = :left
+				@x = @x + TILE_WIDTH
+			elsif @direction == :left
+				@x -= 1
+			end
+		# right run animation & change direction from left to right
+		elsif @window.button_down?(Gosu::KbRight) && @x < (@window.width - TILE_WIDTH * 2)
+			if @direction == :left
+				@direction = :right
+				@x = @x - TILE_WIDTH
+			elsif @direction == :right
+				@x += 1
+			end
+		end
+		@current_frame += 1
+
+		if @current_frame == 26
+			@current_frame = 23
 		end
 	end
 
 	def draw
-		@sprite.draw(@x, @y, 1, -1)
+		if @direction == :left
+			@animation[@current_frame].draw(@x, @y, 1, -1)
+		elsif @direction == :right
+			@animation[@current_frame].draw(@x, @y, 1)
+		end
 	end
+
+	private
 
 end
 
@@ -31,7 +73,7 @@ class GameWindow < Gosu::Window
   def initialize(width = 640, height = 640, fullscreen = false)
     super
     self.caption = "zZzZzZz"
-    @background_image = Gosu::Image.new("town-dungeon.png", :tileable => true)
+    @background_image = Gosu::Image.new('media/town-dungeon.png', :tileable => true)
     @sprite = Sprite.new(self)
   end
 
@@ -40,6 +82,7 @@ class GameWindow < Gosu::Window
   end
 
   def update
+  	# binding.pry
   	@sprite.update
   end
 
