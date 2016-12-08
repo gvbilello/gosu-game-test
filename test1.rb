81ba9589d1ad36a465040880e3f9fd3b9b4ddb28
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-12-07 23:38:53 -0500

    Working on sprite running animation.

--- test1.rb
+++ test1.rb
@@ -1,28 +1,54 @@
 require 'gosu'
 
-class TestGame < Gosu::Window
+class Sprite
 
-  def initialize(width = 800, height = 600, fullscreen = false)
+	def initialize(window)
+		@window = window
+		@sprite = Gosu::Image.new(@window, "characters.png")
+		@x = @window.width / 2
+		@y = @window.height - 66
+		@direction = :right
+	end
+
+	def update
+		if @window.button_down?(Gosu::KbLeft)
+			@direction = :left
+			@x -= 5
+		elsif @window.button_down?(Gosu::KbRight)
+			@direction = :right
+			@x += 5
+		end
+	end
+
+	def draw
+		@sprite.draw(@x, @y, 1, -1)
+	end
+
+end
+
+class GameWindow < Gosu::Window
+
+  def initialize(width = 640, height = 640, fullscreen = false)
     super
     self.caption = "zZzZzZz"
-    @image = Gosu::Image.from_text(self, "zZzZzZz", Gosu.default_font_name, 100)
+    @background_image = Gosu::Image.new("town-dungeon.png", :tileable => true)
+    @sprite = Sprite.new(self)
   end
 
   def button_down(id)
-  	close if id = Gosu::KbEscape
+  	close if id == Gosu::KbEscape
   end
 
   def update
-  	@x = self.width / 2 - @image.width / 2 + Math.sin(Time.now.to_f) * 150
-  	@y = self.height / 2 - @image.height / 2 + Math.cos(Time.now.to_f) * 200
+  	@sprite.update
   end
 
   def draw
-  	@image.draw(@x, @y, 0)
+  	@background_image.draw(0, 0, 0)
+  	@sprite.draw
   end
 
 end
 
-
-window = TestGame.new
+window = GameWindow.new
 window.show
